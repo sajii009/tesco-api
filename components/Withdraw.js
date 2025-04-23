e0a41400edd1a0143e2e7b9bcfd19b023eb397b6
usman-ali-2000 <usmanzulfiqar14@gmail.com>
2025-04-23 16:18:49 +0500

    msg

---
+++ components/Withdraw.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const WithdrawSchema = new mongoose.Schema({
+    sender: {
+        type: String,
+    },
+    receiver: {
+        type: String,
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    bank: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        default: 0
+    },
+    pending: {
+        type: Boolean,
+        default: true
+    },
+    scam: {
+        type: Boolean,
+        default: false,
+    },
+    charges: {
+        type: Number,
+        default: 0
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Withdraw = mongoose.model('Withdraw', WithdrawSchema);
+
+module.exports = Withdraw;
---
+++ components/register.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+
+const registerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  generatedId: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  referalCode: {
+    type: String,
+  },
+  deposit: {
+    type: Number,
+    default: 0
+  },
+  balance: {
+    type: Number,
+    default: 0
+  },
+  totalDeposit: {
+    type: Number,
+    default: 0
+  },
+  totalInvest: {
+    type: Number,
+    default: 0
+  },
+  totalWithdraw: {
+    type: Number,
+    default: 0
+  },
+  totalCommission: {
+    type: Number,
+    default: 0
+  },
+  level1Commission: {
+    type: Number,
+    default: 0
+  },
+  level2Commission: {
+    type: Number,
+    default: 0
+  },
+  level3Commission: {
+    type: Number,
+    default: 0
+  },
+  profileImage: {
+    type: String,
+  },
+  ban: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
+
+const Register = mongoose.model('Register', registerSchema);
+
+module.exports = Register;
